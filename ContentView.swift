5b01851b9cd3c2587eaa8e72a26d49f43f4dd46f
Lunaar000 <calamitasamogus@gmail.com>
2026-05-04 12:27:26 -0500

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- ContentView.swift
+++ ContentView.swift
@@ -11,8 +11,75 @@ struct ContentView: View {
                         Rectangle()
                             .stroke(.yellow, lineWidth: 5)
                     )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                Rectangle()
+                    .fill(Color.purple)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .overlay(
+                        Rectangle()
+                            .stroke(.yellow, lineWidth: 5)
+                    )
+                    .frame(width: 100, height: 100)
+                    .background(.purple)
+
+                    
+
             }
+            
         }
+        .frame(minWidth: 0, idealWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(.white)
     }
 }
 
