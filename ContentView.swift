5a13fa2a63ddaa0b88d18d28fef42b94b5d21cbe
Henry Giurato <hgiurato9133@stu.d214.org>
2026-05-08 11:31:51 -0500

    Removing The Rectangles

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Event = ""
-    @State var startTime: Int
-    @State var endTime: Int
+    @State var startTime: Int = 0
+    @State var endTime: Int = 0
+    @State var event = ""
+    @State var enteredEvent = ""
+    @State var editing = "no"
     var body: some View {
         VStack {
             
@@ -15,6 +17,7 @@ struct ContentView: View {
                         .overlay(
                             Rectangle()
                                 .stroke(.yellow, lineWidth: 5)
+                            
                         )
                     Text("Sat")
                         .font(.title)
@@ -94,95 +97,44 @@ struct ContentView: View {
                 }
             }
             HStack{
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-            }
-            HStack{
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
+                ZStack{
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .overlay(
+                            Rectangle()
+                                .stroke(.yellow, lineWidth: 5)
+                        )
+                    VStack{
+                    Text("\(event)")
+                        if editing == "yes"{
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundStyle(.white)
+                                .onTapGesture {
+                                    event = "\(enteredEvent)"
+                                    editing = "no"
+                                }
+                        }
+                    }
+                }
+                ZStack{
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .overlay(
+                            Rectangle()
+                                .stroke(.yellow, lineWidth: 5)
+                        )
+                    Text("\(event)")
+                    if editing == "yes"{
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundStyle(.white)
+                            .onTapGesture {
+                                event = "\(enteredEvent)"
+                                editing = "no"
+                            }
+                    }
+                }
                 Rectangle()
                     .fill(Color.purple)
                     .frame(width: 100, height: 100, alignment: .center)
@@ -233,147 +185,19 @@ struct ContentView: View {
                     .frame(width: 100, height: 100)
                     .background(.purple)
             }
-            HStack{
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
+            if editing == "no" {
+                Button("Edit", systemImage: "plus.circle.fill") {
+                    editing = "yes"
+                }
+                .foregroundStyle(.black)
+                .padding()
+            }else{
+                TextField("Enter Event", text: $enteredEvent)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .padding()
             }
-            HStack{
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-                
-                Rectangle()
-                    .fill(Color.purple)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .overlay(
-                        Rectangle()
-                            .stroke(.yellow, lineWidth: 5)
-                    )
-                    .frame(width: 100, height: 100)
-                    .background(.purple)
-            }
-        }
+    }
         .frame(minWidth: 0, idealWidth: 0, maxWidth: .infinity, minHeight: 0, idealHeight: 0, maxHeight: .infinity, alignment: .center)
         .background(.white)
     }
